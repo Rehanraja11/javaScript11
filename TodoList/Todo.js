eed608364b55366b20df4638c61fc85ff4505a47
Rehann <rehanraja3725@gmail.com>
2025-12-18 16:49:36 +0530

    init

---
+++ TodoList/Todo.js
@@ -0,0 +1,30 @@
+
+let currentKey = null;
+
+submit.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    let titleC = title.value;
+    let DescriptionC = desc.value;
+
+    currentKey = JSON.stringify(titleC);   //
+    localStorage.setItem(currentKey, JSON.stringify(DescriptionC));
+
+    showData.innerHTML = `
+        <h1>${titleC}</h1>
+        <h3>${DescriptionC}</h3>
+    `;
+
+    title.value = "";
+    desc.value = "";
+});
+
+deleteBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if (currentKey) {
+        localStorage.removeItem(currentKey);
+        showData.innerHTML = "";
+        currentKey = null;
+    }
+});
