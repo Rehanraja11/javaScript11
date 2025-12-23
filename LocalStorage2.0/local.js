b04d665a81de2819ccd682aae43f5c07d355eb05
Rehann <rehanraja3725@gmail.com>
2025-12-23 17:34:08 +0530

    AllbyRehann

--- LocalStorage2.0/local.js
+++ LocalStorage2.0/local.js
@@ -1,32 +1,23 @@
 localStorage.setItem(1, "Rehann Raja")
 localStorage.setItem("Name1", "Rehann11")
 let b = localStorage.getItem("Name")
-console.log(b)
+// console.log(b)
 localStorage.removeItem("Name1")
 localStorage.clear()
 
 
 // with Arrays
-// let namessssss = prompt("Enter The Name")
-
-
-let allnames = ["Ronaldo", "Virat", "Dhoni", "Jethalal"]
-localStorage.setItem("BestNames", JSON.stringify(allnames))
-
-
-let getNames = JSON.parse(localStorage.getItem("BestNames")) || []
-// getNames.push(namessssss)
-
-localStorage.setItem("bestNames", JSON.stringify(getNames))
-
-let recover = JSON.parse(localStorage(get))
-console.log(recover)
 
 
 
+let allNames = ["Rehann", "Abbas", "Soheb", "Alfaix", "Faizal"]
+localStorage.setItem("Names", JSON.stringify(allNames))
+
+
+let recoverName = JSON.parse(localStorage.getItem("Names"))
+console.log(recoverName)
 
 // With Object
-
 // let user = {
 //     name: "soyab",
 //     age: 25,
@@ -35,7 +26,5 @@ console.log(recover)
 // localStorage.setItem("user", JSON.stringify(user));
 
 
-
 // let storedUser = JSON.parse(localStorage.getItem("user"));
-
 // console.log(storedUser);
